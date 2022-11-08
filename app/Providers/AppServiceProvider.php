8813b119b4d08f502592d801729ba06f15715266
volodkaly <lysvl@hotmail.cz>
2022-11-08 10:25:27 +0100

    гейт

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Access\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.app')
 @section ('content')
 {{"Let's cook something amazing! Share your knoledge with the world!"}}
+{{"Latest recipes:"}}
+@foreach ($recs as $rec)
+            <tr>
+                <td>
+                    <h3>{{ $rec->title }}</h3>
+                </td>
+                <td>
+                <h3>{{ $rec->content }}</h3>
+                </td>
+            </tr>
+            @endforeach
 @endsection
